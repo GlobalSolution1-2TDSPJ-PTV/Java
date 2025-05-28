d474b63c16a112f91bd813727e33c8d9be259170
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-28 08:55:22 -0300

    feat: add project config

---
+++ src/main/java/com/global/FloodWatch/FloodWatchApplication.java
@@ -0,0 +1,13 @@
+package com.global.FloodWatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloodWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloodWatchApplication.class, args);
+	}
+
+}
