206362358a0d9c2aa6dfdde0e5f97772e14e25c7
vinioc <viniciusoliveiracoutinho@gmail.com>
2025-06-02 02:25:28 -0300

    feat: start config swagger openAPI

--- src/main/java/com/global/FloodWatch/config/OpenApiConfig.java
+++ src/main/java/com/global/FloodWatch/config/OpenApiConfig.java
@@ -1,4 +1,28 @@
 package com.global.FloodWatch.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("FloodWatch API")
+                        .version("v0.0.1")
+                        .description("API para o sistema FloodWatch de prevenção e resposta a enchentes.")
+                        .termsOfService("http://swagger.io/terms/") // Opcional
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")) // Opcional
+                        .contact(new Contact()
+                                .email("contato@floodwatch.com")
+                                .name("Equipe FloodWatch")
+                                .url("http://www.floodwatch.com")) // Opcional
+                );
+    }
 }
